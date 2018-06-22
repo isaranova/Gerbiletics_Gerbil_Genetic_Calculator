72062d4569d5bdd707be45229622f926d373dd12
Ivana Saranova <i.saranova@seznam.cz>
2018-06-22 02:54:47 +0200

    Corrected gene_combinations function in Gene class, should be working properly now with '-' letters Added minor edit in probability function Added letter_possibilities function for '-' letters and gene_possibilities dictionary for the function Some small tests at the end of the file - TO BE MOVED TO TESTS FILE

--- gerbiletics_source.py
+++ gerbiletics_source.py
@@ -7,6 +7,15 @@ gene_types = {
     'p': ['P', 'p']
 }
 
+gene_possibilities = {
+    'a': ['A.A', 'A.a', 'a.a'],
+    'c': ['C.C', 'C.ch', 'C.chm', 'ch.ch', 'chm.ch', 'chm.chm'],
+    'd': ['D.D', 'D.d', 'd.d'],
+    'e': ['E.E', 'E.e', 'E.ef', 'e.e', 'e.ef', 'ef.ef'],
+    'g': ['G.G', 'G.g', 'g.g'],
+    'p': ['P.P', 'P.p', 'p.p'],
+}
+
 def swap_string(string1, string2):
     temp = string1
     string1 = string2
@@ -20,8 +29,25 @@ def probability(struct, item):
     if i_number == 0:
         return 0
 
-    return i_number / number * 100
+    probability = i_number / number * 100
+    return probability
 
+def letter_possibilities(type, gene):
+    gen1, gen2 = gene.split('.')
+
+    if gen1 == '-' and gen2 == '-':
+        return gene_possibilities[type]
+
+    elif gen1 == '-' or gen2 == '-':
+        possibilities = list()
+        gene = gene.replace('-', '')
+        for p in gene_possibilities[type]:
+            if gene in p:
+                possibilities.extend(p.split('.'))
+        return possibilities
+
+    else:
+        return [gen1, gen2]
 
 class Gene:
     def __init__(self, name, type):
@@ -35,34 +61,35 @@ class Gene:
             self.one, self.two = swap_string(self.one, self.two)
 
         if self.type == ('e' or 'c'):
-            if self.two == gene_types[self.type][2] and self.one == gene_types[self.type][1]:
+            if self.two == gene_types[self.type][1] and self.one == gene_types[self.type][2]:
                 self.one, self.two = swap_string(self.one, self.two)
-                
+
         self.name = self.one + '.' + self.two
 
     def gene_combinations(self, gen2):
         combinations = dict()
         genotypes = list()
 
-        gen1 = [self.one, self.two]
-        gen2 = [gen2.one, gen2.two]
+        gen1 = letter_possibilities(self.type, self.name)
+        gen2 = letter_possibilities(gen2.type, gen2.name)
 
         for i in gen1:
             for y in gen2:
-                if (gen1 or gen2) == '-':
-                    gen = list()
-                    for letter in gene_types[self.type]:
-                        g = Gene(gen1+'.'+gen2, self.type)
-                        g.name.replace('-', letter)
-                        g.letter_position_correction()
-                        gen.append(g)
-			
-                gen = Gene(str(gen1+'.'+gen2), self.type)
-                gen.letter_position_correction()
-                genotypes.append(gen)
+                gen = Gene(i+'.'+y, self.type)
+                genotypes.append(gen.name)
 
         for g in genotypes:
             if g not in combinations:
                 combinations[g] = probability(genotypes, g)
-                
+
         return combinations
+
+gene = Gene('a.A', 'A')
+gen2 = Gene('a.a', 'a')
+funky = Gene('A.-', 'a')
+c = Gene('C.ch', 'c')
+c2 = Gene('ch.chm', 'c')
+combo = gene.gene_combinations(gen2)
+combo2 = gene.gene_combinations(funky)
+combo3 = c.gene_combinations(c2)
+print(combo, combo2, combo3)
