edc833444615dc5ac3bb4144d76ce5c2773224d4
Ivana Saranová <isaranov@localhost.localdomain>
2018-07-20 16:06:06 +0200

    Connected genotype to phenotype and vice versa

--- gerbiletics_gui_pyqt.py
+++ gerbiletics_gui_pyqt.py
@@ -4,11 +4,11 @@ from PyQt5 import uic
 from gerbiletics_source import *
 from support_file import genotype_phenotype
 
-if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+"""if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 
 if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
-    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+    QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)"""
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType("gerbiletics_gui.ui")
 
@@ -43,8 +43,8 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.ui.e_dad.currentTextChanged.connect(self.show_mom_dad_phenotype)
         self.ui.g_dad.currentTextChanged.connect(self.show_mom_dad_phenotype)
         self.ui.p_dad.currentTextChanged.connect(self.show_mom_dad_phenotype)
-
-
+        self.ui.phen_mom.activated.connect(self.show_mom_genotype)
+        self.ui.phen_dad.activated.connect(self.show_dad_genotype)
 
     def show_mom_dad_phenotype(self):
         mom = Genotype(create_genotype(str(self.ui.a_mom.currentText()),
@@ -78,6 +78,39 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             index = self.ui.phen_dad.findText('Unknown')
             self.ui.phen_dad.setCurrentIndex(index)
 
+    def show_mom_genotype(self):
+        genotype_mom = Genotype(genotype_phenotype[self.ui.phen_mom.currentText()])
+
+        ui_genes_mom = [self.ui.a_mom, 
+                        self.ui.c_mom, 
+                        self.ui.d_mom,
+                        self.ui.e_mom,
+                        self.ui.g_mom, 
+                        self.ui.p_mom
+        ]
+        
+
+        for i in range(6):
+            g = genotype_mom.genes[i].name.replace('.', ' ')
+            index = ui_genes_mom[i].findText(g)
+            ui_genes_mom[i].setCurrentIndex(index)
+    
+    def show_dad_genotype(self):
+        genotype_dad = Genotype(genotype_phenotype[self.ui.phen_dad.currentText()])
+        
+        ui_genes_dad = [self.ui.a_dad,
+                        self.ui.c_dad,
+                        self.ui.d_dad,
+                        self.ui.e_dad,
+                        self.ui.g_dad,
+                        self.ui.p_dad
+        ]
+        
+        for i in range(6):
+            g = genotype_dad.genes[i].name.replace('.', ' ')
+            index = ui_genes_dad[i].findText(g)
+            ui_genes_dad[i].setCurrentIndex(index)
+        
 
 
 if __name__ == "__main__":
